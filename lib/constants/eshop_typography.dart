ef8e2be53e4be7d0ade814cb8f856054e4baa9b9
Odarteifx <williamlamp512@gmail.com>
2024-08-23 23:57:14 +0000

    modified

--- lib/constants/eshop_typography.dart
+++ lib/constants/eshop_typography.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EshopTypography {
-  static  double heading1 = 32.0.sp;
-  static  double heading2 = 22.0.sp;
-  static  double onboadingbody = 16.0.sp;
-  static  double signbody = 14.0.sp;
-  static  double termsfont = 12.0.sp;
+  static double heading1 = 32.0.sp;
+  static double heading2 = 22.0.sp;
+  static double onboadingbody = 16.0.sp;
+  static double signbody = 14.0.sp;
+  static double termsfont = 12.0.sp;
   static double homepagecategories = 10.0.sp;
 }
