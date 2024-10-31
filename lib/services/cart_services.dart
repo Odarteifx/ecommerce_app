62175409c669839d1e61ba13f831175122b74a29
Odarteifx <williamlamp512@gmail.com>
2024-10-31 22:22:32 +0000

    cart services

--- lib/services/cart_services.dart
+++ lib/services/cart_services.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/models/cart_item.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class CartServices {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -12,5 +11,8 @@ class CartServices {
   Future<void> addToCart(CartItem item) async {
     await _firestore.collection('users').doc(userId).collection('cart').add(item.toMap());
   }
-  
+
+  Future<void> removeFromCart(String itemId) async{
+    await _firestore.collection('users').doc(userId).collection('cart').doc(itemId).delete();
+  }
 }
